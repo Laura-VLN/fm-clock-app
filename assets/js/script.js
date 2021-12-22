d2021c22bd9902618b77624961189689e54c0876
Laura-VLN <lauravilain.pro@gmail.com>
2021-12-22 16:36:26 +0100

    started quote style - mobile

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+/*let timezone = document.getElementById('timezone')
+
+fetch('http://worldtimeapi.org/api/ip')
+.then(res => res.json())
+.then(data => timezone.innerHTML = timezone.innerHTML + data.timezone)*/
+
+let quote = document.getElementById('quote')
+let author = document.getElementById('author')
+
+fetch('https://programming-quotes-api.herokuapp.com/quotes/random')
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+    quote.innerHTML = data.en
+    author.innerHTML = data.author
+})
